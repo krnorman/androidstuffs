a745251620ea803b8d3a8cee74a22cee495cda9b
kelly.norman <kelly.norman@cvent.com>
2017-12-01 15:38:50 -0700

    Renamed MainActivity to HomeActivity, moved all ButtonActivity class stuff to HomeActivity

---
+++ app/src/main/java/test/security/kn/com/securitytest/HomeActivity.java
@@ -0,0 +1,67 @@
+package test.security.kn.com.securitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.GpioCallback;
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.Gpio;
+
+import java.io.IOException;
+
+public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "HomeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PeripheralManagerService service = new PeripheralManagerService();
+        Log.d(TAG, "Available GPIO: " + service.getGpioList());
+        for (String name : service.getGpioList()) {
+            try {
+                Gpio mButtonGpio = service.openGpio(name);
+                mButtonGpio.setDirection(Gpio.DIRECTION_IN);
+                mButtonGpio.setEdgeTriggerType(Gpio.EDGE_BOTH);
+                mButtonGpio.registerGpioCallback(mCallback);
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+
+        }
+        //setContentView(R.layout.activity_main);
+    }
+
+    private GpioCallback mCallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            Log.i(TAG, "GPIO changed");
+            return true;
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        PeripheralManagerService service = new PeripheralManagerService();
+        Log.d(TAG, "Available GPIO: " + service.getGpioList());
+        for (String name : service.getGpioList()) {
+            try {
+                Gpio mButtonGpio = service.openGpio(name);
+                if (mButtonGpio != null) {
+                    mButtonGpio.unregisterGpioCallback(mCallback);
+                    try {
+                        mButtonGpio.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error on PeripheralIO API", e);
+                    }
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+
+        }
+
+    }
+}
